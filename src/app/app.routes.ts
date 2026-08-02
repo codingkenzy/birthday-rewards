6d1198b374a239ff373666e2de22a66a6c26ceb7
codingkenzy <limosenerodk1998@gmail.com>
2026-08-02 17:45:08 +0800

    feat: initialize Birthday Rewards project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Countdown } from './features/countdown/pages/countdown/countdown';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Countdown
+  }
+];
---
+++ src/app/features/countdown/pages/countdown/countdown.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-countdown',
+  imports: [],
+  templateUrl: './countdown.html',
+  styleUrl: './countdown.scss',
+})
+export class Countdown {}
